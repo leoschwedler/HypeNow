fbd122c6a26dae068c34a08066d78a405909e4e8
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2025-08-21 16:04:57 -0300

    feat(di): configure GetIt and register controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hype_now/src/app/app_widget.dart';
+import 'package:hype_now/app_widget.dart';
+import 'package:hype_now/src/dependencies/injector.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await injector();
   runApp(const AppWidget());
 }
---
+++ lib/src/dependencies/injector.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:hype_now/src/ui/controllers/theme_controller.dart';
+import 'package:hype_now/src/ui/controllers/splash_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> injector() async {
+  getIt.registerSingleton<ThemeController>(
+    ThemeController(sharedPreferences: await SharedPreferences.getInstance()),
+  );
+  getIt.registerFactory<SplashController>(() => SplashController());
+}
